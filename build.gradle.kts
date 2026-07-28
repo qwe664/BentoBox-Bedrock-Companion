ded0976d5b81b81fea8f938a48005dad4153bf8d
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-28 20:18:31 +0800

    Add repositories and dependencies for PaperMC

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,24 @@ plugins {
 
 group = project.property("group")
 version = project.property("version")
+
+repositories {
+    mavenCentral()
+
+    // Paper / Purpur
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.2-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+}

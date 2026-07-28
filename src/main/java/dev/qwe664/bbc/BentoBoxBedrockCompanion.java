3deefbcf7db6145e7521d4f13c6777e1bbef8d6e
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-28 21:07:50 +0800

    Add FloodgateHook and FormManager to BentoBoxBedrockCompanion

--- src/main/java/dev/qwe664/bbc/BentoBoxBedrockCompanion.java
+++ src/main/java/dev/qwe664/bbc/BentoBoxBedrockCompanion.java
@@ -1,16 +1,32 @@
 package dev.qwe664.bbc;
 
+import dev.qwe664.bbc.hook.FloodgateHook;
+import dev.qwe664.bbc.manager.FormManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BentoBoxBedrockCompanion extends JavaPlugin {
 
+    private FloodgateHook floodgateHook;
+    private FormManager formManager;
+
     @Override
     public void onEnable() {
+        floodgateHook = new FloodgateHook();
+        formManager = new FormManager();
+
         getLogger().info("BentoBox Bedrock Companion has been enabled!");
     }
 
     @Override
     public void onDisable() {
         getLogger().info("BentoBox Bedrock Companion has been disabled!");
+    }
+
+    public FloodgateHook getFloodgateHook() {
+        return floodgateHook;
+    }
+
+    public FormManager getFormManager() {
+        return formManager;
     }
 }

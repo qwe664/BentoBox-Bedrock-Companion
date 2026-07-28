eb33dee49a82ec6902e024a6c7f31f8accd14845
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-28 21:06:26 +0800

    Implement isBedrock method in FloodgateHook
    Add method to check if a player is a Bedrock player using Floodgate API.

--- src/main/java/dev/qwe664/bbc/hook/FloodgateHook.java
+++ src/main/java/dev/qwe664/bbc/hook/FloodgateHook.java
@@ -1,4 +1,17 @@
 package dev.qwe664.bbc.hook;
 
+import org.bukkit.entity.Player;
+import org.geysermc.floodgate.api.FloodgateApi;
+
 public class FloodgateHook {
+
+    /**
+     * 判斷玩家是否為 Bedrock 玩家
+     *
+     * @param player Bukkit 玩家
+     * @return true = Bedrock，false = Java
+     */
+    public boolean isBedrock(Player player) {
+        return FloodgateApi.getInstance().isFloodgatePlayer(player.getUniqueId());
+    }
 }

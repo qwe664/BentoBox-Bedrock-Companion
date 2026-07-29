38ef39263b3b5f9968e9e8968677a0667a099be4
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-29 13:00:09 +0800

    Refactor FormManager to include MainMenuForm

--- src/main/java/dev/qwe664/bbc/manager/FormManager.java
+++ src/main/java/dev/qwe664/bbc/manager/FormManager.java
@@ -1,17 +1,24 @@
 package dev.qwe664.bbc.manager;
 
 import dev.qwe664.bbc.BentoBoxBedrockCompanion;
-import org.geysermc.cumulus.form.SimpleForm;
+import dev.qwe664.bbc.form.MainMenuForm;
+import org.bukkit.entity.Player;
 
 public class FormManager {
 
     private final BentoBoxBedrockCompanion plugin;
+    private final MainMenuForm mainMenuForm;
 
     public FormManager(BentoBoxBedrockCompanion plugin) {
         this.plugin = plugin;
+        this.mainMenuForm = new MainMenuForm(plugin);
     }
 
-    public void test() {
-        var form = SimpleForm.builder();
+    public BentoBoxBedrockCompanion getPlugin() {
+        return plugin;
+    }
+
+    public void showMainMenu(Player player) {
+        mainMenuForm.open(player);
     }
 }

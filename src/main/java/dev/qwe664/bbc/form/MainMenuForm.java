1c96272482b11aa98f4bea0e5c35f05796b4de0f
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-29 12:57:18 +0800

    Extend MainMenuForm from BaseForm and add constructor

--- src/main/java/dev/qwe664/bbc/form/MainMenuForm.java
+++ src/main/java/dev/qwe664/bbc/form/MainMenuForm.java
@@ -1,5 +1,15 @@
 package dev.qwe664.bbc.form;
 
-public class MainMenuForm {
+import dev.qwe664.bbc.BentoBoxBedrockCompanion;
+import org.bukkit.entity.Player;
 
+public class MainMenuForm extends BaseForm {
+
+    public MainMenuForm(BentoBoxBedrockCompanion plugin) {
+        super(plugin);
+    }
+
+    public void open(Player player) {
+
+    }
 }

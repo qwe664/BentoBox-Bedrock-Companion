e3adc0c2a0a09da713fff895030c78949d62a62b
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-29 12:17:55 +0800

    Create BaseForm class for form handling

---
+++ src/main/java/dev/qwe664/bbc/form/BaseForm.java
@@ -0,0 +1,12 @@
+package dev.qwe664.bbc.form;
+
+import dev.qwe664.bbc.BentoBoxBedrockCompanion;
+
+public abstract class BaseForm {
+
+    protected final BentoBoxBedrockCompanion plugin;
+
+    protected BaseForm(BentoBoxBedrockCompanion plugin) {
+        this.plugin = plugin;
+    }
+}

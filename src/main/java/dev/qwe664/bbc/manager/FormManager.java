1ff9ef12f8af08b6aef9e260b23851dbb477810a
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-29 11:27:30 +0800

    Add constructor to FormManager with plugin dependency

--- src/main/java/dev/qwe664/bbc/manager/FormManager.java
+++ src/main/java/dev/qwe664/bbc/manager/FormManager.java
@@ -1,4 +1,12 @@
 package dev.qwe664.bbc.manager;
 
+import dev.qwe664.bbc.BentoBoxBedrockCompanion;
+
 public class FormManager {
+
+    private final BentoBoxBedrockCompanion plugin;
+
+    public FormManager(BentoBoxBedrockCompanion plugin) {
+        this.plugin = plugin;
+    }
 }

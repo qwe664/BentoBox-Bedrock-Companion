19daa4b23bd56e33937b889e03b61b9640e4f090
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-29 16:48:46 +0800

    Add DebugMenuForm to FormManager

--- src/main/java/dev/qwe664/bbc/manager/FormManager.java
+++ src/main/java/dev/qwe664/bbc/manager/FormManager.java
@@ -1,6 +1,7 @@
 package dev.qwe664.bbc.manager;
 
 import dev.qwe664.bbc.BentoBoxBedrockCompanion;
+import dev.qwe664.bbc.form.DebugMenuForm;
 import dev.qwe664.bbc.form.IslandMenuForm;
 import dev.qwe664.bbc.form.MainMenuForm;
 import org.bukkit.entity.Player;
@@ -9,10 +10,12 @@ public class FormManager {
 
     private final MainMenuForm mainMenuForm;
     private final IslandMenuForm islandMenuForm;
+    private final DebugMenuForm debugMenuForm;
 
     public FormManager(BentoBoxBedrockCompanion plugin) {
         this.mainMenuForm = new MainMenuForm(plugin);
         this.islandMenuForm = new IslandMenuForm(plugin);
+        this.debugMenuForm = new DebugMenuForm(plugin);
     }
 
     public void openMainMenu(Player player) {
@@ -21,5 +24,9 @@ public class FormManager {
 
     public void openIslandMenu(Player player) {
         islandMenuForm.open(player);
+    }
+
+    public void openDebugMenu(Player player) {
+        debugMenuForm.open(player);
     }
 }

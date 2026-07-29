17f6b1e794e43e7eafe4d86f184c9b05b434b8ff
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-29 21:12:44 +0800

    Add AdminMenuForm to FormManager

--- src/main/java/dev/qwe664/bbc/manager/FormManager.java
+++ src/main/java/dev/qwe664/bbc/manager/FormManager.java
@@ -1,6 +1,7 @@
 package dev.qwe664.bbc.manager;
 
 import dev.qwe664.bbc.BentoBoxBedrockCompanion;
+import dev.qwe664.bbc.form.AdminMenuForm;
 import dev.qwe664.bbc.form.DebugMenuForm;
 import dev.qwe664.bbc.form.IslandMenuForm;
 import dev.qwe664.bbc.form.MainMenuForm;
@@ -11,11 +12,13 @@ public class FormManager {
     private final MainMenuForm mainMenuForm;
     private final IslandMenuForm islandMenuForm;
     private final DebugMenuForm debugMenuForm;
+    private final AdminMenuForm adminMenuForm;
 
     public FormManager(BentoBoxBedrockCompanion plugin) {
         this.mainMenuForm = new MainMenuForm(plugin);
         this.islandMenuForm = new IslandMenuForm(plugin);
         this.debugMenuForm = new DebugMenuForm(plugin);
+        this.adminMenuForm = new AdminMenuForm(plugin);
     }
 
     public void openMainMenu(Player player) {
@@ -29,5 +32,8 @@ public class FormManager {
     public void openDebugMenu(Player player) {
         debugMenuForm.open(player);
     }
-    public void openAdminMenu(Player player)
+
+    public void openAdminMenu(Player player) {
+        adminMenuForm.open(player);
+    }
 }

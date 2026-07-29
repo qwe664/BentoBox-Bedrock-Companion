a90d2a3a83a11f8cb9c49c9448eb1bab73ab3783
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-07-29 16:01:12 +0800

    Create IslandService class for plugin management

---
+++ src/main/java/dev/qwe664/bbc/service/IslandService.java
@@ -0,0 +1,12 @@
+package dev.qwe664.bbc.service;
+
+import dev.qwe664.bbc.BentoBoxBedrockCompanion;
+
+public class IslandService {
+
+    private final BentoBoxBedrockCompanion plugin;
+
+    public IslandService(BentoBoxBedrockCompanion plugin) {
+        this.plugin = plugin;
+    }
+}

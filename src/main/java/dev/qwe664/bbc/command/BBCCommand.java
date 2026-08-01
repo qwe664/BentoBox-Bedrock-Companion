a4b973752eb23a80ba712f811550c5da9b7210c1
qwe664 <93234700+qwe664@users.noreply.github.com>
2026-08-01 18:12:24 +0800

    Refactor BBCCommand to use DebugCommand class

--- src/main/java/dev/qwe664/bbc/command/BBCCommand.java
+++ src/main/java/dev/qwe664/bbc/command/BBCCommand.java
@@ -1,20 +1,19 @@
 package dev.qwe664.bbc.command;
 
 import dev.qwe664.bbc.BentoBoxBedrockCompanion;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 
 public class BBCCommand implements CommandExecutor {
 
     private final BentoBoxBedrockCompanion plugin;
+    private final DebugCommand debugCommand;
 
     public BBCCommand(BentoBoxBedrockCompanion plugin) {
         this.plugin = plugin;
+        this.debugCommand = new DebugCommand(plugin);
     }
 
     @Override
@@ -27,41 +26,11 @@ public class BBCCommand implements CommandExecutor {
 
         // /bbc debug
         if (args.length > 0 && args[0].equalsIgnoreCase("debug")) {
-
-            player.sendMessage(ChatColor.GOLD + "===== BBC Debug =====");
-
-            player.sendMessage(status("BBC", plugin));
-
-            player.sendMessage(status("BentoBox",
-                    Bukkit.getPluginManager().getPlugin("BentoBox")));
-
-            player.sendMessage(status("Floodgate",
-                    Bukkit.getPluginManager().getPlugin("floodgate")));
-
-            player.sendMessage(status("Geyser",
-                    Bukkit.getPluginManager().getPlugin("Geyser-Spigot")));
-
-            player.sendMessage(status("PlaceholderAPI",
-                    Bukkit.getPluginManager().getPlugin("PlaceholderAPI")));
-
-            player.sendMessage(ChatColor.GOLD + "=====================");
-
-            return true;
+            return debugCommand.execute(player, args);
         }
 
         // /bbc
         plugin.getFormManager().openMainMenu(player);
-
         return true;
-    }
-
-    private String status(String name, Plugin plugin) {
-
-        if (plugin == null) {
-            return ChatColor.RED + "✘ " + name + ": Not Installed";
-        }
-
-        return ChatColor.GREEN + "✔ " + name + ": "
-                + plugin.getDescription().getVersion();
     }
 }
